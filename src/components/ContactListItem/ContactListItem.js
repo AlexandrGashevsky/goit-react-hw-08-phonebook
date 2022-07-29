de578bf046b88418f7526f15f1fba580b77f52e7
AlexandrGashevsky <71451324+AlexandrGashevsky@users.noreply.github.com>
2022-07-30 01:28:24 +0300

    c

--- src/components/ContactListItem/ContactListItem.js
+++ src/components/ContactListItem/ContactListItem.js
@@ -10,24 +10,6 @@ import { useDeleteContactsMutation } from '../../redux/contactSlice';
 const ContactListItem = ({ id, name, number }) => {
   const [deleteContact] = useDeleteContactsMutation();
 
-  function stringToColor(string) {
-    let hash = 0;
-    let i;
-
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color = '#';
-
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff;
-      color += `00${value.toString(16)}`.slice(-2);
-    }
-
-    return color;
-  }
-
   return (
     <>
       <div className={contactListItemStyles.item}>
